e24dc785c0c0dc506ea51a36ca3b31801fa0f899
G1.Jeon <g1.tommy.jeon@gmail.com>
2022-02-15 10:22:19 +0900

    feat: Initialize Backend Boilerplate

---
+++ backend/src/main/java/us/stcorp/team3/hackathonproject/HackathonProjectApplication.java
@@ -0,0 +1,13 @@
+package us.stcorp.team3.hackathonproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathonProjectApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/us/stcorp/team3/hackathonproject/HackathonProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package us.stcorp.team3.hackathonproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
