f23ebc2fba67e966d8a1b2aabcc2400fefae538d
MinChul-Son <smc5236@gmail.com>
2022-02-22 17:26:26 +0900

    ⚙️ fix: 테스트에는 임시로 빈 yaml 적용

--- backend/src/main/java/us/stcorp/team3/hackathonproject/HackathonProjectApplication.java
+++ backend/src/main/java/us/stcorp/team3/hackathonproject/HackathonProjectApplication.java
@@ -2,8 +2,10 @@ package us.stcorp.team3.hackathonproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
+@Profile("dev")
 public class HackathonProjectApplication {
 
 	public static void main(String[] args) {
--- backend/src/test/java/us/stcorp/team3/hackathonproject/HackathonProjectApplicationTests.java
+++ backend/src/test/java/us/stcorp/team3/hackathonproject/HackathonProjectApplicationTests.java
@@ -2,8 +2,10 @@ package us.stcorp.team3.hackathonproject;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootTest
+@Profile("test")
 class HackathonProjectApplicationTests {
 
 	@Test
