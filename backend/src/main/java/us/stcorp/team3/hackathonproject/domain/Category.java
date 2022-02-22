f9acb8d114d5043992c3861ff96ee292328bac62
jihyun park <girl9505@naver.com>
2022-02-22 14:47:21 +0900

    feat: Category 도메인 구현

---
+++ backend/src/main/java/us/stcorp/team3/hackathonproject/domain/Category.java
@@ -0,0 +1,43 @@
+package us.stcorp.team3.hackathonproject.domain;
+
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+@NoArgsConstructor
+@Getter
+@ToString
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, insertable = false, updatable = false, columnDefinition = "datetime DEFAULT CURRENT_TIMESTAMP")
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @Setter
+    @Column(nullable = false, insertable = false, updatable = false, columnDefinition = "datetime DEFAULT CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP")
+    private String createdBy;
+    @Column(nullable = false)
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
+    @Setter
+    @Column(nullable = false)
+    private String modifiedBy;
+
+}
