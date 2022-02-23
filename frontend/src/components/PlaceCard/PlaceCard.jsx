f90a7cb7c679a3f6fc7d886ad7574574919d7a3e
Dami Kim <dami.k.dev@gmail.com>
2022-02-23 13:39:48 +0900

    feat: PlaceCard에 상세페이지 라우팅 추가 (#18)

--- frontend/src/components/PlaceCard/PlaceCard.jsx
+++ frontend/src/components/PlaceCard/PlaceCard.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
@@ -21,25 +22,29 @@ const PlaceCard = () => {
 
   return (
     <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image="https://w.namu.la/s/09a0cf2a5bee5446d5a740fcd11e12001af5c926a01419944fefc7abebbb26fd74c9110da6b44700f70162158c4163c5a2d99045d5babb70a376d40fabd7e2de34973df5785e9c5eee4a7c8d56a62306"
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            맥도날드
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            어쩌고 저쩌고 맥날 베토디 상하이 냠냠
-          </Typography>
-        </CardContent>
-      </CardActionArea>
+      <Link to="/place">
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            image="https://w.namu.la/s/09a0cf2a5bee5446d5a740fcd11e12001af5c926a01419944fefc7abebbb26fd74c9110da6b44700f70162158c4163c5a2d99045d5babb70a376d40fabd7e2de34973df5785e9c5eee4a7c8d56a62306"
+            title="Contemplative Reptile"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              맥도날드
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              어쩌고 저쩌고 맥날 베토디 상하이 냠냠
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Link>
       <CardActions>
-        <Button size="small" color="primary">
-          지도보기
-        </Button>
+        <a href="http://naver.me/GTgSdco0">
+          <Button size="small" color="primary">
+            지도보기
+          </Button>
+        </a>
         <Button size="small" color="primary">
           리뷰쓰기
         </Button>
