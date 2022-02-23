40f26b476e71c7108c939bcb840fbcb626d2bc7c
MinChul-Son <smc5236@gmail.com>
2022-02-23 14:22:29 +0900

    ⚙️ feat: jpa audition 애노테이션 추가 - #22
    - 애플리케이션에서 사용하기 위해서 추가

--- backend/src/main/java/us/stcorp/team3/hackathonproject/HackathonProjectApplication.java
+++ backend/src/main/java/us/stcorp/team3/hackathonproject/HackathonProjectApplication.java
@@ -2,8 +2,10 @@ package us.stcorp.team3.hackathonproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class HackathonProjectApplication {
 
     public static void main(String[] args) {
