52c9add6577e95f05b5ea613b78b9e40ce93cf2c
jihyun park <girl9505@naver.com>
2022-02-23 10:18:13 +0900

    style: 전체 코드 정렬 resolves #6

--- backend/src/main/java/us/stcorp/team3/hackathonproject/HackathonProjectApplication.java
+++ backend/src/main/java/us/stcorp/team3/hackathonproject/HackathonProjectApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HackathonProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HackathonProjectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HackathonProjectApplication.class, args);
+    }
 
 }
