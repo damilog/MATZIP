7bc40a274ca936f497b58fd64af2f9fb13f4f8f1
MinChul-Son <smc5236@gmail.com>
2022-02-25 03:07:58 +0900

    ✨ refactor: 맛집 DTO 스펙 변경 - #72
    - 해당 맛집 작성자 반환

--- backend/src/main/java/us/stcorp/team3/hackathonproject/dto/MatzipResponse.java
+++ backend/src/main/java/us/stcorp/team3/hackathonproject/dto/MatzipResponse.java
@@ -7,7 +7,7 @@ import us.stcorp.team3.hackathonproject.domain.Matzip;
 public record MatzipResponse(Long id, String title, String content, Integer viewCount,
                              String thumbnail, String naverUrl, Double naverRating,
                              Long naverComment, String address, Integer price, Category category,
-                             Double rating,
+                             Double rating, String createdBy,
                              List<MatzipReviewResponse> review) {
 
     public static MatzipResponse mapToMatzipResponse(final Matzip matzip,
@@ -15,6 +15,7 @@ public record MatzipResponse(Long id, String title, String content, Integer view
         return new MatzipResponse(matzip.getId(), matzip.getTitle(), matzip.getContent(),
             matzip.getViewCount(), matzip.getThumbnail(), matzip.getNaverUrl(),
             matzip.getNaverRating(), matzip.getNaverComment(), matzip.getAddress(),
-            matzip.getPrice(), matzip.getCategory(), matzip.getRating(), reviews);
+            matzip.getPrice(), matzip.getCategory(), matzip.getRating(), matzip.getCreatedBy(),
+            reviews);
     }
 }
