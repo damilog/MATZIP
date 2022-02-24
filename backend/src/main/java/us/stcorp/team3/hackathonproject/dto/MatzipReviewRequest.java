91382844869a9bcd6d9882f3b1294ebf19ec0afd
jihyun park <girl9505@naver.com>
2022-02-24 23:39:11 +0900

    style: google style code 정렬

--- backend/src/main/java/us/stcorp/team3/hackathonproject/dto/MatzipReviewRequest.java
+++ backend/src/main/java/us/stcorp/team3/hackathonproject/dto/MatzipReviewRequest.java
@@ -5,7 +5,8 @@ import us.stcorp.team3.hackathonproject.domain.MatzipReview;
 
 public record MatzipReviewRequest(String content, Integer rating) {
 
-    public static MatzipReview mapToMatzipReview(Matzip matzip, MatzipReviewRequest matzipReviewRequest) {
+    public static MatzipReview mapToMatzipReview(Matzip matzip,
+        MatzipReviewRequest matzipReviewRequest) {
         String anonymousAuthorForReview = String.valueOf(matzipReviewRequest.content().hashCode());
         return MatzipReview.of(matzipReviewRequest.content, anonymousAuthorForReview,
             anonymousAuthorForReview, matzip, matzipReviewRequest.rating);
