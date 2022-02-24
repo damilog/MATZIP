bbb04ab7d5d7a5f1903cd660fd13138905ab2805
Dami Kim <dami.k.dev@gmail.com>
2022-02-25 05:05:30 +0900

    feat: editor, home, place Store 구현

--- frontend/src/store/homeStore.js
+++ frontend/src/store/homeStore.js
@@ -4,3 +4,13 @@ export const placeDataAtom = atom({
   key: 'placeDataAtom',
   default: [],
 });
+
+export const filterAtom = atom({
+  key: 'filterAtom',
+  default: [],
+});
+
+export const clickedPlaceIdAtom = atom({
+  key: 'clickedPlaceIdAtom',
+  default: 0,
+});
