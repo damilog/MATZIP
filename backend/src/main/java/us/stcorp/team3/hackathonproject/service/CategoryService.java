bf42a3da5330b511e6cb5bcc4cefc22d19b235fb
jihyun park <girl9505@naver.com>
2022-02-24 15:56:31 +0900

    feat: category 항목들을 조회하는 비즈니스 로직 추가
    - stream을 사용하여 한번에 List로 반환해준다.

--- backend/src/main/java/us/stcorp/team3/hackathonproject/service/CategoryService.java
+++ backend/src/main/java/us/stcorp/team3/hackathonproject/service/CategoryService.java
@@ -1,2 +1,20 @@
-package us.stcorp.team3.hackathonproject.service;public class CategoryService {
+package us.stcorp.team3.hackathonproject.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import us.stcorp.team3.hackathonproject.dto.CategoryResponse;
+import us.stcorp.team3.hackathonproject.repository.CategoryRepository;
+
+@RequiredArgsConstructor
+@Service
+public class CategoryService {
+
+    private CategoryRepository categoryRepository;
+
+    public List<CategoryResponse> findCategories() {
+        return categoryRepository.findAll().stream().map(CategoryResponse::categoryToDto)
+            .collect(Collectors.toList());
+    }
 }
