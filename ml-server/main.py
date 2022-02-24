cf440f4b66ac4599c359c5377a0af1526041c033
G1.Jeon <g1.tommy.jeon@gmail.com>
2022-02-24 14:54:44 +0900

    feat: Server API structure (FastAPI) - #4
    - main.py (Endpoint: [POST] /recommend/{item_id})
    - Port Number 8000

--- ml-server/main.py
+++ ml-server/main.py
@@ -1,10 +1,33 @@
-from typing import Optional
+import os
 
 from fastapi import FastAPI
 
+from utils.MLHelper import MLHelper
+from utils.DBHelper import DBHelper
+
 app = FastAPI()
 
+envOptions = dict(user=os.environ['user'], passwd=os.environ['passwd'], host=os.environ['host'], db=os.environ['db'], charset=os.environ['charset'])
+statusOk = "ok"
 
-@app.get("/recommend")
-def read_root():
-    return {"Hello": "World"}
+clustersPath = './utils/data.pickle'
+modelPath = './utils/kmode_model.pkl'
+
+@app.get("/recommend/{item_id}")
+async def read_root(item_id: int):
+    dataHeaders = [1, 2, 3]
+
+    dbHelper = DBHelper(envOptions)
+    mlHelper = MLHelper(clustersPath, modelPath)
+
+    itemDict = dbHelper.getItem(item_id)
+
+    row = mlHelper.toDataFrame(itemDict)
+    mlHelper.label(row, dataHeaders)
+
+    recommendItems = mlHelper.recommends(row)
+    responses = dbHelper.getResponses(recommendItems)
+
+    dbHelper.close()
+
+    return dict(status=statusOk, count=len(responses), items=responses)
