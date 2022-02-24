0d772e6ba97d9ff03a2e61bb3ffd9b5372bcb7f8
jihyunPark <girl9505@naver.com>
2022-02-25 03:07:46 +0900

    refactor: 맛집 리뷰 등록시 맛집 평점에 반영 (#67) (#74)

--- backend/src/main/java/us/stcorp/team3/hackathonproject/service/MatzipReviewService.java
+++ backend/src/main/java/us/stcorp/team3/hackathonproject/service/MatzipReviewService.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import us.stcorp.team3.hackathonproject.domain.Matzip;
+import us.stcorp.team3.hackathonproject.domain.QMatzip;
 import us.stcorp.team3.hackathonproject.dto.MatzipReviewRequest;
 import us.stcorp.team3.hackathonproject.repository.MatzipRepository;
 import us.stcorp.team3.hackathonproject.repository.ReviewRepository;
@@ -21,10 +22,20 @@ public class MatzipReviewService {
     private final MatzipRepository matzipRepository;
     private final EntityManager entityManager;
 
+    @Transactional
     public void saveMatzipReview(Long matzipId, MatzipReviewRequest matzipReviewRequest) {
         Matzip matzip = matzipRepository.findById(matzipId)
             .orElseThrow(() -> new IllegalArgumentException("no such data"));
         reviewRepository.save(MatzipReviewRequest.mapToMatzipReview(matzip, matzipReviewRequest));
+
+        QMatzip qMatzip = QMatzip.matzip;
+        JPAQueryFactory queryFactory = new JPAQueryFactory(entityManager);
+        Double ratingAverage = queryFactory.select(matzipReview.rating.avg())
+            .from(matzipReview).fetchOne();
+        queryFactory.update(qMatzip)
+            .set(qMatzip.rating, ratingAverage)
+            .where(qMatzip.id.eq(matzipId))
+            .execute();
     }
 
     @Transactional
