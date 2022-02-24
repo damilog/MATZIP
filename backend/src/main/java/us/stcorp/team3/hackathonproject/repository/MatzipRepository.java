7b28582f4af1a9761ef15cb6504c93bea45b72ee
jihyun park <girl9505@naver.com>
2022-02-25 02:36:14 +0900

    feat: 리파지토리 queryDsl 적용 (#68)

--- backend/src/main/java/us/stcorp/team3/hackathonproject/repository/MatzipRepository.java
+++ backend/src/main/java/us/stcorp/team3/hackathonproject/repository/MatzipRepository.java
@@ -3,10 +3,12 @@ package us.stcorp.team3.hackathonproject.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import us.stcorp.team3.hackathonproject.domain.Category;
 import us.stcorp.team3.hackathonproject.domain.Matzip;
 
-public interface MatzipRepository extends JpaRepository<Matzip, Long> {
+public interface MatzipRepository extends JpaRepository<Matzip, Long>,
+    QuerydslPredicateExecutor<Matzip> {
 
     Page<Matzip> findAll(Pageable pageable);
 
