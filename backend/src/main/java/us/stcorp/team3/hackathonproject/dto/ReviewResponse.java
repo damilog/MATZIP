40a64aa50b8d6609bdfde4d511e4e5a10f54f276
MinChul-Son <smc5236@gmail.com>
2022-02-24 22:39:39 +0900

    ✨ feat: ReviewDTO 생성 #43

---
+++ backend/src/main/java/us/stcorp/team3/hackathonproject/dto/ReviewResponse.java
@@ -0,0 +1,11 @@
+package us.stcorp.team3.hackathonproject.dto;
+
+import us.stcorp.team3.hackathonproject.domain.MatzipReview;
+
+public record ReviewResponse(Long id, String comment, Integer rating) {
+
+    public static ReviewResponse matToReviewResponse(final MatzipReview review) {
+        return new ReviewResponse(review.getId(), review.getComment(), review.getRating());
+    }
+
+}
