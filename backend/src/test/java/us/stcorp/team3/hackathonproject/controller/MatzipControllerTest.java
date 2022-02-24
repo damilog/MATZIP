5b68a8f2eb65a9337600e2020144bcc5ea6a66cd
MinChul-Son <smc5236@gmail.com>
2022-02-24 14:55:49 +0900

    ✨ refactor: 변경된 로직에 따른 테스트 코드 변경 - #26

--- backend/src/test/java/us/stcorp/team3/hackathonproject/controller/MatzipControllerTest.java
+++ backend/src/test/java/us/stcorp/team3/hackathonproject/controller/MatzipControllerTest.java
@@ -3,6 +3,7 @@ package us.stcorp.team3.hackathonproject.controller;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -15,10 +16,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import us.stcorp.team3.hackathonproject.domain.Matzip;
@@ -30,6 +28,7 @@ class MatzipControllerTest {
 
     private final MockMvc mvc;
     List<MatzipResponse> matzipResponses;
+    List<MatzipResponse> expectedResult;
     PageRequest pageRequest;
 
     @MockBean
@@ -41,16 +40,43 @@ class MatzipControllerTest {
 
     @BeforeEach
     void setUp() {
-        pageRequest = PageRequest.of(0, 12, Sort.by(Direction.DESC, "rating"));
+        pageRequest = PageRequest.of(0, 12);
 
         List<Matzip> matzips = List.of(
-            Matzip.of("루피네 한식당", "루피가 요리해줌", 1, 3.5f,
-                "www.11st.co.kr", 3.5f, 12L, "www.11st.co.kr",
+            Matzip.of("루피네 한식당", "루피가 요리해줌", 1, 4.5f,
+                "www.11st.co.kr", 4.5f, 12L, "www.11st.co.kr",
                 "서울스퀘어", "2만원대", "민철", "민철"),
-            Matzip.of("루피네 외식당", "루피가 요리해줌", 13, 3.5f,
-                "www.11st.co.kr", 3.5f, 12L, "www.11st.co.kr",
+            Matzip.of("루피네 외식당", "루피가 요리해줌", 13, 2.5f,
+                "www.11st.co.kr", 2.5f, 12L, "www.11st.co.kr",
                 "서울스퀘어", "2만원대", "민철", "민철"),
             Matzip.of("루피네 양식당", "루피가 요리해줌", 15, 3.5f,
+                "www.11st.co.kr", 3.5f, 12L, "www.11st.co.kr",
+                "서울스퀘어", "2만원대", "민철", "민철"),
+            Matzip.of("루피네 중식당", "루피가 요리해줌", 5, 3.5f,
+                "www.11st.co.kr", 3.5f, 12L, "www.11st.co.kr",
+                "서울스퀘어", "2만원대", "민철", "민철"),
+            Matzip.of("루피네 중식당", "루피가 요리해줌", 5, 3.5f,
+                "www.11st.co.kr", 3.5f, 12L, "www.11st.co.kr",
+                "서울스퀘어", "2만원대", "민철", "민철"),
+            Matzip.of("루피네 중식당", "루피가 요리해줌", 5, 3.5f,
+                "www.11st.co.kr", 3.5f, 12L, "www.11st.co.kr",
+                "서울스퀘어", "2만원대", "민철", "민철"),
+            Matzip.of("루피네 중식당", "루피가 요리해줌", 5, 3.5f,
+                "www.11st.co.kr", 3.5f, 12L, "www.11st.co.kr",
+                "서울스퀘어", "2만원대", "민철", "민철"),
+            Matzip.of("루피네 중식당", "루피가 요리해줌", 5, 3.5f,
+                "www.11st.co.kr", 3.5f, 12L, "www.11st.co.kr",
+                "서울스퀘어", "2만원대", "민철", "민철"),
+            Matzip.of("루피네 중식당", "루피가 요리해줌", 5, 3.5f,
+                "www.11st.co.kr", 3.5f, 12L, "www.11st.co.kr",
+                "서울스퀘어", "2만원대", "민철", "민철"),
+            Matzip.of("루피네 중식당", "루피가 요리해줌", 5, 3.5f,
+                "www.11st.co.kr", 3.5f, 12L, "www.11st.co.kr",
+                "서울스퀘어", "2만원대", "민철", "민철"),
+            Matzip.of("루피네 중식당", "루피가 요리해줌", 5, 3.5f,
+                "www.11st.co.kr", 3.5f, 12L, "www.11st.co.kr",
+                "서울스퀘어", "2만원대", "민철", "민철"),
+            Matzip.of("루피네 중식당", "루피가 요리해줌", 5, 3.5f,
                 "www.11st.co.kr", 3.5f, 12L, "www.11st.co.kr",
                 "서울스퀘어", "2만원대", "민철", "민철"),
             Matzip.of("루피네 중식당", "루피가 요리해줌", 5, 3.5f,
@@ -61,26 +87,70 @@ class MatzipControllerTest {
             .map(matzip -> MatzipResponse.matToRecord(matzip))
             .collect(Collectors.toList());
 
+        List<Matzip> expected = List.of(
+            Matzip.of("루피네 한식당", "루피가 요리해줌", 1, 4.5f,
+                "www.11st.co.kr", 4.5f, 12L, "www.11st.co.kr",
+                "서울스퀘어", "2만원대", "민철", "민철"),
+            Matzip.of("루피네 양식당", "루피가 요리해줌", 15, 3.5f,
+                "www.11st.co.kr", 3.5f, 12L, "www.11st.co.kr",
+                "서울스퀘어", "2만원대", "민철", "민철"),
+            Matzip.of("루피네 중식당", "루피가 요리해줌", 5, 3.5f,
+                "www.11st.co.kr", 3.5f, 12L, "www.11st.co.kr",
+                "서울스퀘어", "2만원대", "민철", "민철"),
+            Matzip.of("루피네 중식당", "루피가 요리해줌", 5, 3.5f,
+                "www.11st.co.kr", 3.5f, 12L, "www.11st.co.kr",
+                "서울스퀘어", "2만원대", "민철", "민철"),
+            Matzip.of("루피네 중식당", "루피가 요리해줌", 5, 3.5f,
+                "www.11st.co.kr", 3.5f, 12L, "www.11st.co.kr",
+                "서울스퀘어", "2만원대", "민철", "민철"),
+            Matzip.of("루피네 중식당", "루피가 요리해줌", 5, 3.5f,
+                "www.11st.co.kr", 3.5f, 12L, "www.11st.co.kr",
+                "서울스퀘어", "2만원대", "민철", "민철"),
+            Matzip.of("루피네 중식당", "루피가 요리해줌", 5, 3.5f,
+                "www.11st.co.kr", 3.5f, 12L, "www.11st.co.kr",
+                "서울스퀘어", "2만원대", "민철", "민철"),
+            Matzip.of("루피네 중식당", "루피가 요리해줌", 5, 3.5f,
+                "www.11st.co.kr", 3.5f, 12L, "www.11st.co.kr",
+                "서울스퀘어", "2만원대", "민철", "민철"),
+            Matzip.of("루피네 중식당", "루피가 요리해줌", 5, 3.5f,
+                "www.11st.co.kr", 3.5f, 12L, "www.11st.co.kr",
+                "서울스퀘어", "2만원대", "민철", "민철"),
+            Matzip.of("루피네 중식당", "루피가 요리해줌", 5, 3.5f,
+                "www.11st.co.kr", 3.5f, 12L, "www.11st.co.kr",
+                "서울스퀘어", "2만원대", "민철", "민철"),
+            Matzip.of("루피네 중식당", "루피가 요리해줌", 5, 3.5f,
+                "www.11st.co.kr", 3.5f, 12L, "www.11st.co.kr",
+                "서울스퀘어", "2만원대", "민철", "민철"),
+            Matzip.of("루피네 중식당", "루피가 요리해줌", 5, 3.5f,
+                "www.11st.co.kr", 3.5f, 12L, "www.11st.co.kr",
+                "서울스퀘어", "2만원대", "민철", "민철"));
+
+        expectedResult = expected.stream()
+            .map(matzip -> MatzipResponse.matToRecord(matzip))
+            .collect(Collectors.toList());
+
+
     }
 
     @DisplayName("[GET] /matzip?page={} - 전체 맛집 조회(한 페이지당 12개씩")
     @Test
     void getAllMatzip_Use_Paging() throws Exception {
         // given
-        given(matzipService.findAllMatzip(0)).willReturn(
-            new PageImpl<>(matzipResponses, pageRequest, matzipResponses.size()));
+        given(matzipService.findAllMatzip(pageRequest)).willReturn(expectedResult);
 
         // when
         mvc.perform(
-                get("/api/matzip")
+                get("/api/matzip?page=0")
             )
             .andExpect(status().isOk())
             .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-            .andExpect(jsonPath("$.content.length()").value(4))
-            .andExpect(jsonPath("$.content[0].title", "루피네 한식당").exists())
-            .andExpect(jsonPath("$.content[1].title", "루피네 외식당").exists());
+            .andExpect(jsonPath("length()").value(12))
+            .andExpect(jsonPath("$.[?(@.title == '%s')]", "루피네 한식당").exists())
+            .andExpect(jsonPath("$.[?(@.title == '%s')]", "루피네 양식당").exists())
+            .andExpect(jsonPath("$.[?(@.title == '%s')]", "루피네 외식당").doesNotExist())
+            .andDo(print());
 
         // then
-        then(matzipService).should().findAllMatzip(0);
+        then(matzipService).should().findAllMatzip(pageRequest);
     }
 }
