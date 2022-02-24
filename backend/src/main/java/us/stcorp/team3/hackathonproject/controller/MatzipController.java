ecb41d7e38c8f608bda194ea3ca54032a1d05f56
MinChul-Son <smc5236@gmail.com>
2022-02-25 00:37:53 +0900

    ✨ feat: 맛집 수정 컨트롤러 구현- #46

--- backend/src/main/java/us/stcorp/team3/hackathonproject/controller/MatzipController.java
+++ backend/src/main/java/us/stcorp/team3/hackathonproject/controller/MatzipController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -50,6 +51,13 @@ public class MatzipController {
     @GetMapping("/{matzipId}")
     public ResponseEntity<MatzipResponse> findMatzip(@PathVariable long matzipId) {
         return ResponseEntity.ok(matzipService.findMatzip(matzipId));
+    }
+
+    @PutMapping("/{matzipId}")
+    public ResponseEntity<String> modifyMatzip(@PathVariable long matzipId,
+        @RequestBody MatzipRequest matzipRequest) {
+        matzipService.modifyMatzip(matzipId, matzipRequest);
+        return ResponseEntity.ok("Your Request has Succeed");
     }
 
     @DeleteMapping("/{matzipId}")
