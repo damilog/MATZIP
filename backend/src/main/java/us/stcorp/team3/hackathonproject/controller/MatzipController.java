ccaeef495795ae81bc791243ff0f04859f2ed968
MinChul-Son <smc5236@gmail.com>
2022-02-24 14:57:34 +0900

    ✨ refactor: 컨트롤러 반환타입 명시 - #26

--- backend/src/main/java/us/stcorp/team3/hackathonproject/controller/MatzipController.java
+++ backend/src/main/java/us/stcorp/team3/hackathonproject/controller/MatzipController.java
@@ -1,13 +1,14 @@
 package us.stcorp.team3.hackathonproject.controller;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import us.stcorp.team3.hackathonproject.dto.MatzipResponse;
 import us.stcorp.team3.hackathonproject.service.MatzipService;
 
 @RestController
@@ -18,7 +19,7 @@ public class MatzipController {
     private final MatzipService matzipService;
 
     @GetMapping
-    public ResponseEntity<Object> allMatzip(@PageableDefault(size = 12) Pageable pageable) {
+    public ResponseEntity<List<MatzipResponse>> allMatzip(@PageableDefault(size = 12) Pageable pageable) {
         return ResponseEntity.ok(matzipService.findAllMatzip(pageable));
     }
 }
