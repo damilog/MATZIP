8a14047cd4d4571bb77b01aba49d63b21f7ee909
MinChul-Son <smc5236@gmail.com>
2022-02-24 19:38:16 +0900

    ✨ refactor: 메서드 네이밍 오타 수정

--- backend/src/main/java/us/stcorp/team3/hackathonproject/dto/EntireMatzipResponse.java
+++ backend/src/main/java/us/stcorp/team3/hackathonproject/dto/EntireMatzipResponse.java
@@ -5,7 +5,7 @@ import us.stcorp.team3.hackathonproject.domain.Matzip;
 public record EntireMatzipResponse(Long id, String title, String content, String thumbnail, Float rating,
                                    String price) {
 
-    public static EntireMatzipResponse matToRecord(final Matzip matzip) {
+    public static EntireMatzipResponse mapToRecord(final Matzip matzip) {
         return new EntireMatzipResponse(matzip.getId(), matzip.getTitle(), matzip.getContent(),
             matzip.getThumbnail(), matzip.getRating(), matzip.getPrice());
     }
