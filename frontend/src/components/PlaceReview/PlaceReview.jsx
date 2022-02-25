af772458b7d9341fd11313bf8c82c0663e073193
Dami Kim <dami.k.dev@gmail.com>
2022-02-25 15:09:27 +0900

    fix: review remove 버튼 삭제 (#52

--- frontend/src/components/PlaceReview/PlaceReview.jsx
+++ frontend/src/components/PlaceReview/PlaceReview.jsx
@@ -2,18 +2,10 @@ import { useRecoilValue } from 'recoil';
 import { placeDetailDataAtom } from 'store/placeStore';
 import styled from 'styles/themedComponents';
 import StarRating from 'components/common/StarRating';
-import CancelIcon from '@mui/icons-material/Cancel';
-import API from 'util/API';
 
 const PlaceReview = ({ placeId }) => {
   const detailData = useRecoilValue(placeDetailDataAtom);
   const { review } = detailData;
-
-  const handleRemoveButtonClick = async (reviewId) => {
-    if (window.confirm('리뷰를 삭제하시겠습니까?')) {
-      API.deleteReview(placeId, reviewId);
-    }
-  };
 
   const Reviews = review?.map(({ id, comment, rating }) => (
     <ReviewList key={id}>
@@ -21,7 +13,6 @@ const PlaceReview = ({ placeId }) => {
         <StarRating rating={rating} />
       </div>
       <DescriptionLayer>{comment}</DescriptionLayer>
-      <RemoveButton onClick={() => handleRemoveButtonClick(id)} />
     </ReviewList>
   ));
 
@@ -45,15 +36,6 @@ const DescriptionLayer = styled.div`
 
   margin-bottom: 15px;
   padding: 10px;
-`;
-
-const RemoveButton = styled(CancelIcon)`
-  color: ${({ theme }) => theme.colors.gray.light};
-  margin-bottom: 10px;
-  cursor: pointer;
-  &:hover {
-    color: ${({ theme }) => theme.colors.red.main};
-  }
 `;
 
 export default PlaceReview;
