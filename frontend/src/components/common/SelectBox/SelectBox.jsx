0a5cc929168c7b66a58b88775282d14d146429da
Dami Kim <dami.k.dev@gmail.com>
2022-02-25 12:02:19 +0900

    feat: SelectBox filter API적용 (#80)

--- frontend/src/components/common/SelectBox/SelectBox.jsx
+++ frontend/src/components/common/SelectBox/SelectBox.jsx
@@ -2,9 +2,9 @@ import styled from 'styles/themedComponents';
 import NativeSelect from '@material-ui/core/NativeSelect';
 
 const SelectBox = ({ selectArray, width, name, onChange }) => {
-  const options = selectArray.map(({ id, name }) => (
-    <option key={id} value={name}>
-      {name}
+  const options = selectArray.map((option, id) => (
+    <option key={id} value={option}>
+      {option}
     </option>
   ));
 
