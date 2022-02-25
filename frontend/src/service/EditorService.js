a9b83220234634ff9f2cdf3f3b49e55a5efb73e7
Dami Kim <dami.k.dev@gmail.com>
2022-02-25 13:13:03 +0900

    feat: 맛집 추가 기능 구현 (#80)

--- frontend/src/service/EditorService.js
+++ frontend/src/service/EditorService.js
@@ -24,12 +24,22 @@ const EditorService = () => {
   const handlePlaceInputChange = (e) => {
     const { name, value } = e.target;
     console.log(name, value);
+    let newPlace = {};
 
-    const newPlace = {
-      ...placeData,
-      [name]: value,
-    };
+    if ((name === 'naverComment') | (name === 'naverRating') | (name === 'price')) {
+      newPlace = {
+        ...placeData,
+        [name]: Number(value),
+      };
+    } else {
+      newPlace = {
+        ...placeData,
+        [name]: value,
+      };
+    }
+
     setPlaceData(newPlace);
+    console.log(placeData);
   };
 
   const checkEmptyInput = () => {
@@ -41,9 +51,21 @@ const EditorService = () => {
     checkEmptyInput();
 
     if (isEmpty) return;
-    console.log(placeData);
-    console.log('?');
-    //API.postPlace(placeData);
+    console.log('ㄷ', placeData);
+    const test = {
+      title: '맥도날드 서울역점',
+      content: '빅맥',
+      thumbnail: 'https://live.staticflickr.com/7829/45936066114_b6eb7812a6_b.jpg',
+      naverRating: 4.39,
+      naverComment: 9433,
+      naverUrl: 'http://naver.me/5OnfFQMB',
+      address: '서울 용산구 한강대로 405 서울역(철도역)',
+      price: 6000,
+      category: '양식',
+      username: '다미',
+    };
+
+    API.postPlace(placeData);
     navigate('/');
   };
 
